6c5088c4c70436cecba3e25b3b4dc1e003d8498a
10023402 <10023402@american.fmtusd.org>
2021-12-02 13:57:35 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let lightTheme = "styles/light.css";
+let darkTheme = "styles/dark.css";
+
+// Clears the screen on click of C button.
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+// Displays entered value on screen.
+function liveScreen(value) {
+  let res = document.getElementById("result");
+  if(res.value == "undefined"){
+    res.value = "";
+  }
+  res.value += value;
+}
+
